e6a179ff0ca8f90428963148405c40a548623056
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-04-29 21:58:35 +0500

    add some blaaa

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* <Router /> */}
-      <h1>Sorry, Web is under process</h1>
+      <h1>Sorry, Olx clone is under process</h1>
     </div>
   );
 };
